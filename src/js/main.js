3216ab57d5c799477ff678d57eb71f9550e1bf97
Artem Komarov <imnicon@gmail.com>
2019-11-15 19:56:15 +0900

    add header

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import $ from 'jquery'
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/modal';
+
+$('.mobile-nav .menu-item-has-children > a').click(function () {
+  $(this).next().collapse('toggle')
+  return false
+})
+
+$('#search').on('shown.bs.collapse', function () {
+  $('#search input').focus()
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+
+new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery'
+});
 
 module.exports = {
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist/js'),
   }
-};
+}
