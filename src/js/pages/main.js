6541fbb9d3917c8a01cc07715f9006db80e210e4
Artem Komarov <imnicon@gmail.com>
2019-11-19 00:29:32 +0900

    parteners & news block

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -1,12 +1,30 @@
 import Swiper from 'swiper';
 
 const slider = document.querySelector('.main-slider');
+const partnersSlider = document.querySelector('.partners-slider');
 
 slider && new Swiper(slider, {
-  // autoHeight: 1,
+  loop: 1,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+});
+
+partnersSlider && new Swiper(partnersSlider, {
+  slidesPerView: 2,
+  slidesPerColumn: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: document.querySelector('.partners-slider-nav button:last-child'),
+    prevEl: document.querySelector('.partners-slider-nav button:first-child'),
+  },
+  breakpoints: {
+    1200: {
+      slidesPerColumn: 1,
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  }
 });
 
